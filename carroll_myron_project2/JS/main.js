ff1e18b96706c034a8e8aa34466aa3580153bdd6
Myron Carroll <myronbennell@fullsail.edu>
2012-05-10 16:59:42 -0400

    Add functions
    Store Data and correcting comments

--- carroll_myron_project2/JS/main.js
+++ carroll_myron_project2/JS/main.js
@@ -5,7 +5,8 @@
 alert("Works");
 // Wait until the DOM is ready.
 window.addEventListener("DOMContentLoaded", function(){
-var titleGroups = ["Choose One", "Ms", "Mrs", "Mr"];
+var titleGroups = ["Choose One", "Ms", "Mrs", "Mr"],
+	groupvalue;
 	//GetElementByID Function
 	function $(x){
 		var theElement = document.getElementById(x);
@@ -26,23 +27,33 @@ var titleGroups = ["Choose One", "Ms", "Mrs", "Mr"];
 		}
 		selectLi.appendChild(makeSelect);
 	}
+	//Find value of selected radio button
+	function getSelectedRadio(){
+        var radio = document.forms[0].group;
+        for(var i=0; i < radio.length; i++){
+        if(radio[i].checked){
+                var groupValue = radio[i].value;
+            }
+        }
+    }
 	//Store the data to local storage
 	function storeData(){
         var id = Math.floor(Math.random()*10000000001);
         //Gather up all form field values in an object
         //Object properties contain an array that contains the form label and input value.
+        getSelectedRadio();
         var item          ={};
             item.title    =["Title", $('title').value];
             item.fname    =["First Name", $('fname').value];
             item.lname    =["Last Name", $('lname').value];
             item.email    =["Email", $('email').value];
             item.birth    =["Birthday", $('birth').value];
-//            item.group    =["Group", ];
+            item.group    =["Group", groupValue ];
             item.prayer    =["Prayer Request", $('prayer').value];
             item.needs    =["Needs", $('needs').value];
             //Save the data in local storage: use stringify to convert to a string
             localStorage.setItem(id, JSON.stringify(item));
-            alert("Contact Saved")
+            alert("Contact Saved");
     }
 	//Variable Defaults
 	
@@ -53,8 +64,8 @@ var titleGroups = ["Choose One", "Ms", "Mrs", "Mr"];
 	displayLink.addEventListener("click", getData);
 	var clearLink = $('clear');
 	clearLink.addEventListener('click', clearLocal);
-	var save = $('submit');
-*/	save.addEventListener('click', storeData);
+*/	var save = $('submit');
+	save.addEventListener('click', storeData);
 
 
 
