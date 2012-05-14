9959a5ecd3e3064681949415d6c372c4126f2e64
Myron Carroll <myronbennell@fullsail.edu>
2012-05-14 03:00:57 -0400

    Calling it a night.
    Now Javascript is not working. Will fix tomorrow.

--- carroll_myron_project2/JS/main.js
+++ carroll_myron_project2/JS/main.js
@@ -3,31 +3,32 @@
 // Class: Visual Frameworks
 
 // Wait until the DOM is ready.
+alert("Javascript Works")
 window.addEventListener("DOMContentLoaded", function(){
 var titleGroups = ["Choose One", "Ms", "Mrs", "Mr"],
-	groupValue;
-	//GetElementByID Function
-	function $(x){
-		var theElement = document.getElementById(x);
-		return theElement;
-	}
-	//Create select field element and populate with options.
-	function makeTitle(){
-		var formTag = document.getElementsByTagName("form"),
-			selectLi=$('select'),
-			makeSelect = document.createElement('select');
-			makeSelect.setAttribute("id", "title");
-		for(var i=0, j=titleGroups.length; i<j; i++){
-			var makeOption = document.createElement('option');
-			var optText = titleGroups[i];
-			makeOption.setAttribute("value", optText);
-			makeOption.innerHTML = optText;
-			makeSelect.appendChild(makeOption);
-		}
-		selectLi.appendChild(makeSelect);
-	}
-	//Find value of selected radio button
-	function getSelectedRadio(){
+    groupValue;
+    //GetElementByID Function
+    function $(x){
+        var theElement = document.getElementById(x);
+        return theElement;
+    }
+    //Create select field element and populate with options.
+    function makeTitle(){
+        var formTag = document.getElementsByTagName("form"),
+            selectLi=$('select'),
+            makeSelect = document.createElement('select');
+            makeSelect.setAttribute("id", "title");
+        for(var i=0, j=titleGroups.length; i<j; i++){
+            var makeOption = document.createElement('option');
+            var optText = titleGroups[i];
+            makeOption.setAttribute("value", optText);
+            makeOption.innerHTML = optText;
+            makeSelect.appendChild(makeOption);
+        }
+        selectLi.appendChild(makeSelect);
+    }
+    //Find value of selected radio button
+    function getSelectedRadio(){
         var radio = document.forms[0].group;
         for(var i=0; i < radio.length; i++){
         if(radio[i].checked){
@@ -36,28 +37,28 @@ var titleGroups = ["Choose One", "Ms", "Mrs", "Mr"],
         }
     }
     
-    function toggleControls(n) {
-	switch(n) {
-		case "on":
-			$("minForm").style.display = "none";
-			$("clear").style.display = "inline";
-			$("display").style.display = "none";
-			$("addNew").style.display = "inline";
-			break;
-		case "off":
-			$("minForm").style.display = "block";
-			$("clear").style.display = "inline";
-			$("display").style.display = "inline";
-			$("addNew").style.display = "none";
-			$("items").style.display = "none";
-			break;
-		default:
-			return false;
-	}
+    function toggleControls(n){
+    switch(n) {
+        case "on":
+            $("minForm").style.display = "none";
+            $("clear").style.display = "inline";
+            $("display").style.display = "none";
+            $("addNew").style.display = "inline";
+            break;
+        case "off":
+            $("minForm").style.display = "block";
+            $("clear").style.display = "inline";
+            $("display").style.display = "inline";
+            $("addNew").style.display = "none";
+            $("items").style.display = "none";
+            break;
+        default:
+            return false;
+    }
 };
 
-	//Store the data to local storage
-	function storeData(){
+    //Store the data to local storage
+    function storeData(){
         var id = Math.floor(Math.random()*10000000001);
         //Gather up all form field values in an object
         //Object properties contain an array that contains the form label and input value.
@@ -76,40 +77,52 @@ var titleGroups = ["Choose One", "Ms", "Mrs", "Mr"],
             alert("Contact Saved");
     }
     function getData(){
-	toggleControls("on")
-    	var makeDiv = document.createElement('div');
-    	makeDiv.setAttribute("id", "items");
-    	var makeList = document.createElement('ul');
-    	makeDiv.appendChild(makeList);
-	document.body.appendChild(makeDiv);
-	$("items").style.display = "block"
-    	for(var i=0, len=localStorage.length; i<len; i++){
-    		var makeLi = document.createElement('li');
-    		makeList.appendChild(makeLi);
-    		var key = localStorage.key(i);
-    		var value = localStorage.getItem(key);
-    		var obj = JSON.parse(value);
-		var makeSublist = document.createElement("ul");
-		makeli.appendChild(makeSublist);
-		for (var n in obj) {
-			var makeSubli = document.createElement("li");
-			makeSublist.appendChild(makeSubli);
-			var optSubText = obj[n][0] + " " + obj[n][1];
-			makeSubli.innerHTML = optSubText;
-    		
-    	}
+    toggleControls("on");
+        var makeDiv = document.createElement('div');
+        makeDiv.setAttribute("id", "items");
+        var makeList = document.createElement('ul');
+        makeDiv.appendChild(makeList);
+    document.body.appendChild(makeDiv);
+    $("items").style.display = "block";
+        for(var i=0, len=localStorage.length; i<len; i++){
+            var makeLi = document.createElement('li');
+            makeList.appendChild(makeLi);
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+            var obj = JSON.parse(value);
+        var makeSublist = document.createElement("ul");
+        makeLi.appendChild(makeSublist);
+        for (var n in obj) {
+            var makeSubli = document.createElement("li");
+            makeSublist.appendChild(makeSubli);
+            var optSubText = obj[n][0] + " " + obj[n][1];
+            makeSubli.innerHTML = optSubText;
+            
+        }
     
     }
-	
-	makeTitle();
-	
-	//Set Links and Submit Click Events
-/*	var displayLink = $('display');
-	displayLink.addEventListener("click", getData);
-	var clearLink = $('clear');
-	clearLink.addEventListener('click', clearLocal);
-*/	var save = $('submit');
-	save.addEventListener('click', storeData);
+    
+    
+function clearLocal() {
+    if(localStorage.length === 0) {
+        alert("There is no data to clear.");
+    } else {
+        localStorage.clear();
+        alert("All contacts are deleted.");
+        window.location.reload();
+        return false;    
+    }
+}
+    
+    makeTitle();
+    
+    //Set Links and Submit Click Events
+    var displayLink = $('display');
+    displayLink.addEventListener("click", getData);
+    var clearLink = $('clear');
+    clearLink.addEventListener('click', clearLocal);
+    var save = $('submit');
+    save.addEventListener('click', storeData);
 
 
 
